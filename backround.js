8dad684ada9c1af001b48a101d4d17ceb5df1d36
acridmoon <acridmoon10@gmail.com>
2016-12-06 15:56:09 -0800

    backround.js

---
+++ backround.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	var chance = Math.floor(Math.random() * 100);
+	if(chance === 69) {
+		Subscribe();
+	}
+});
+
+var Subscribe = function() {
+	setTimeout(function() {
+		window.location.href = "http://youtube.com/channel/UCHlDxLyjvZ-CvjyR3IuKxKQ";
+	}, 3000);
+};
